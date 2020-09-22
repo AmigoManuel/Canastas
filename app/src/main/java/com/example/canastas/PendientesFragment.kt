14d56d9029ce17a4cb9601edcd66e5b0f3a2a2ea
AmigoManuel <diazulloamanuel@gmail.com>
2020-09-22 20:57:28 -0300

    Ajustes en de comentarios

--- app/src/main/java/com/example/canastas/PendientesFragment.kt
+++ app/src/main/java/com/example/canastas/PendientesFragment.kt
@@ -15,27 +15,17 @@ import kotlinx.android.synthetic.main.fragment_pendientes.*
 import java.time.LocalDate
 import java.time.LocalTime
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PendientesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PendientesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    // Lista de canastas pendientes manejadas por el fragment
+    private var listaCanastas: ArrayList<Canasta> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        arguments?.let {}
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -43,7 +33,6 @@ class PendientesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pendientes, container, false)
     }
 
@@ -57,7 +46,7 @@ class PendientesFragment : Fragment() {
         // Manejo de clicks en item de la recyclerView
         recyclerViewPendientes.addOnItemClickListener(object: OnItemClickListener {
             override fun onItemClicked(position: Int, view: View) {
-                // TODO: Que hacer al clickear un item
+
             }
         })
     }
@@ -79,8 +68,8 @@ class PendientesFragment : Fragment() {
     fun setCanastasOnView(){
         // Consulta por las canastas en la bd
         val db = DBHandler(requireContext())
-        val listaCanastas = db.db_get_canastasPendientes()
-        println(listaCanastas)
+        // Asigna la lista de canastas en el array local
+        listaCanastas = db.db_get_canastasPendientes()
         // Setea las canastas de la bd dentro del recyclerView
         recyclerViewPendientes.adapter = CanastaAdapter(listaCanastas)
         recyclerViewPendientes.layoutManager = LinearLayoutManager(activity)
@@ -103,15 +92,6 @@ class PendientesFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PendientesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             PendientesFragment().apply {
