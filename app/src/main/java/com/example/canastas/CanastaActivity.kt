a01e6ee9d2841c2fa6b3583abb5f35793578ea20
AmigoManuel <diazulloamanuel@gmail.com>
2020-09-23 16:26:32 -0300

    Modificaciones menores de diseño

--- app/src/main/java/com/example/canastas/CanastaActivity.kt
+++ app/src/main/java/com/example/canastas/CanastaActivity.kt
@@ -1,7 +1,9 @@
 package com.example.canastas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.example.canastas.models.Canasta
 import kotlinx.android.synthetic.main.activity_canasta.*
@@ -17,5 +19,11 @@ class CanastaActivity : AppCompatActivity() {
 
         val canasta = intent.getSerializableExtra("obj_canasta") as Canasta
         nombre_canasta.text = canasta.nombre
+        descripcion_canasta.text = canasta.descripcion
+    }
+
+    // Botón flotante para añadir nueva producto
+    fun nuevoProducto(view: View) {
+        Toast.makeText(this, "añadir producto", Toast.LENGTH_SHORT).show()
     }
 }
